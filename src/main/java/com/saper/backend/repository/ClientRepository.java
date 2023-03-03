48e5c492d8a672b5a8c5cbe07d80b3a50acad13b
Thalyson <thalyon.silva@ifce.edu.br>
2023-03-03 16:12:38 -0300

    Adição de filtro por nome.

--- src/main/java/com/saper/backend/repository/ClientRepository.java
+++ src/main/java/com/saper/backend/repository/ClientRepository.java
@@ -4,7 +4,11 @@ import com.saper.backend.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> findAllByName(String name);
 }
