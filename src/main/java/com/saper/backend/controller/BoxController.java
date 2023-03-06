63b340569e1b694570cc8b825097f0a1f4e1a7f5
Thalyson <thalyon.silva@ifce.edu.br>
2023-03-06 20:27:00 -0300

    Adição do endpoint update no BoxController.

--- src/main/java/com/saper/backend/controller/BoxController.java
+++ src/main/java/com/saper/backend/controller/BoxController.java
@@ -5,6 +5,7 @@ import com.saper.backend.dto.BoxResponseDTO;
 import com.saper.backend.model.Box;
 import com.saper.backend.service.BoxService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,4 +31,12 @@ public class BoxController {
     public BoxRequestDTO save(@RequestBody BoxRequestDTO boxRequestDTO){
         return boxService.save(boxRequestDTO);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(
+            @RequestBody BoxRequestDTO boxRequestDTO,
+            @PathVariable(name = "id") Long id){
+        return boxService.update(id, boxRequestDTO);
+    }
+
 }
--- src/main/java/com/saper/backend/service/BoxService.java
+++ src/main/java/com/saper/backend/service/BoxService.java
@@ -5,9 +5,12 @@ import com.saper.backend.dto.BoxResponseDTO;
 import com.saper.backend.model.Box;
 import com.saper.backend.repository.BoxRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BoxService {
@@ -28,5 +31,25 @@ public class BoxService {
         boxRespository.save(box);
 
         return boxRequestDTO;
+    }
+
+    public ResponseEntity<Object> update(Long id, BoxRequestDTO boxRequestDTO) {
+        Optional<Box> boxOptional = boxRespository.findById(id);
+        if(boxOptional.isPresent()){
+            Box box = boxOptional.get();
+
+            if(boxRequestDTO.getCapacity()!=0){
+                box.setCapacity(boxRequestDTO.getCapacity());
+            }
+            if(boxRequestDTO.getName()!=null){
+                box.setName(boxRequestDTO.getName());
+            }
+
+            BoxResponseDTO boxResponseDTO = new BoxResponseDTO(boxRespository.save(box));
+
+            return ResponseEntity.status(HttpStatus.OK).body(boxResponseDTO);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Box não encontrado");
+        }
     }
 }
