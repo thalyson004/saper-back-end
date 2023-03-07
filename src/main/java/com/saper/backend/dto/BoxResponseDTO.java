740e4c962a7f2434bde29e76a6a13f33a6666889
Thalyson <thalyon.silva@ifce.edu.br>
2023-03-07 20:24:43 -0300

    Alteração do BoxResponseDTO.

--- src/main/java/com/saper/backend/dto/BoxResponseDTO.java
+++ src/main/java/com/saper/backend/dto/BoxResponseDTO.java
@@ -2,6 +2,9 @@ package com.saper.backend.dto;
 
 import com.saper.backend.model.Box;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class BoxResponseDTO {
 
     String name;
@@ -10,10 +13,13 @@ public class BoxResponseDTO {
 
     String created_by;
 
-    public BoxResponseDTO(String name, int capacity, String created_by) {
+    Set<Long> teams;
+
+    public BoxResponseDTO(String name, int capacity, String created_by, Set<Long> teams) {
         this.name = name;
         this.capacity = capacity;
         this.created_by = created_by;
+        this.teams = teams;
     }
 
     public BoxResponseDTO() {
@@ -23,7 +29,15 @@ public class BoxResponseDTO {
         name = box.getName();
         capacity = box.getCapacity();
         created_by = box.getCreated_by();
+        teams = box.getTeams().stream().map(team -> team.getId()).collect(Collectors.toSet());
+    }
 
+    public Set<Long> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Set<Long> teams) {
+        this.teams = teams;
     }
 
     public String getName() {
