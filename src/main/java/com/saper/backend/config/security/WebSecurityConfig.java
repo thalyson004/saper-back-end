fa3bac9a77848bfc19d5074e08c69d097ea78a04
Thalyson <thalyon.silva@ifce.edu.br>
2023-03-08 15:10:52 -0300

    Adição de usuários de acesso em memória.

--- src/main/java/com/saper/backend/config/security/WebSecurityConfig.java
+++ src/main/java/com/saper/backend/config/security/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.saper.backend.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableWebSecurity
@@ -17,4 +21,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
         http.csrf().disable();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER");
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("thalyson")
+                .password("thalyson")
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return rawPassword.equals(encodedPassword);
+            }
+        };
+    }
+
 }
