6eb6991708ef3d004af4cc78985a5d98c93b0768
Thalyson <thalyon.silva@ifce.edu.br>
2023-03-09 20:28:56 -0300

    Adição de role ao adicionar um student.

--- src/main/java/com/saper/backend/controller/MyController.java
+++ src/main/java/com/saper/backend/controller/MyController.java
@@ -1,6 +1,10 @@
 package com.saper.backend.controller;
 
+import com.saper.backend.dto.TeamResponseDTO;
 import com.saper.backend.model.Client;
+import com.saper.backend.model.Student;
+import com.saper.backend.model.Team;
+import com.saper.backend.repository.StudentRepository;
 import com.saper.backend.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,11 +14,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/my")
 public class MyController {
     @Autowired
     ClientService clientService;
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @GetMapping("/client")
     public ResponseEntity<Object> findClient(){
@@ -26,6 +36,28 @@ public class MyController {
         if(principal instanceof Client client) {
             return ResponseEntity.status(HttpStatus.OK)
                     .body(clientService.find(client.getId()).getBody());
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @GetMapping("/enrollments")
+    public ResponseEntity<Object> findEnrollments(){
+        Object principal = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        if(principal instanceof Client client) {
+            Long client_id = client.getId();
+
+            Optional<Student> studentOptional = studentRepository.findByClientId(client_id);
+            List<TeamResponseDTO> teams = studentOptional.get()
+                                        .getTeams().stream()
+                                        .map(TeamResponseDTO::new).toList();
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(teams);
         }else{
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
