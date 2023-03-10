00d0d8eac811c2bf7065a0092f5ae0d980ca7dda
Thalyson <thalyon.silva@ifce.edu.br>
2023-03-10 12:04:44 -0300

    Adição do ErrorDTO

---
+++ src/main/java/com/saper/backend/exception/ErrorDTO.java
@@ -0,0 +1,28 @@
+package com.saper.backend.exception;
+
+
+public class ErrorDTO {
+    String field;
+    String error;
+
+    public ErrorDTO(String field, String error) {
+        this.field = field;
+        this.error = error;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
