ce55e818255cc5264a000ceec0c7010ee4e2b05d
Thalyson <thalyon.silva@ifce.edu.br>
2023-03-10 13:30:24 -0300

    Atualização do ErrorDTO

--- src/main/java/com/saper/backend/exception/ErrorDTO.java
+++ src/main/java/com/saper/backend/exception/ErrorDTO.java
@@ -1,21 +1,37 @@
 package com.saper.backend.exception;
 
 
+import java.time.Instant;
+
 public class ErrorDTO {
-    String field;
+    Instant timeStamp;
+    String status;
     String error;
+    String message;
+    String path;
 
-    public ErrorDTO(String field, String error) {
-        this.field = field;
+    public ErrorDTO(Instant timeStamp, String status, String error, String message, String path) {
+        this.timeStamp = timeStamp;
+        this.status = status;
         this.error = error;
+        this.message = message;
+        this.path = path;
     }
 
-    public String getField() {
-        return field;
+    public Instant getTimeStamp() {
+        return timeStamp;
     }
 
-    public void setField(String field) {
-        this.field = field;
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String getError() {
@@ -24,5 +40,21 @@ public class ErrorDTO {
 
     public void setError(String error) {
         this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
